0aee22b12577c889787bee048bf06d23ce1da69f
sambit-bal <sambit.bal83@gmail.com>
2020-08-23 00:06:09 +0100

    test

--- Identity-Operator.py
+++ Identity-Operator.py
@@ -1,1 +1,10 @@
-# Identity-Operator
+# Identity-Operator (is , is not)
+a= 10
+b= '10'
+print (a is b)
+
+c=20
+d=20
+e='20'
+print (c is not d)
+print (c is not e)
---
+++ type-Conversion.py
@@ -0,0 +1,33 @@
+# type-Conversion : implicit and explicit
+
+## # type-Conversion : implicit 
+
+a= 5
+b= 2
+value = (a/b)
+print(value)
+print(type(value))
+
+d= "sambit"
+e= "bal"
+f= d + e
+print(f)
+print(type(f))
+
+# type-Conversion :  explicit (type cast conversion)
+
+w= 10
+r= 8
+value = w/r
+print(type(value))
+int_value=int(value)
+print(int_value)
+print(type(int_value))
+
+o=20
+p='10'
+print(type(p))
+l=o+int(p)
+print(l)
+print(type(l))
+
