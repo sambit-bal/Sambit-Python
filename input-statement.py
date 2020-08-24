18f643d90bedf59ecdd1d2326663ecec3f536207
sambit-bal <sambit.bal83@gmail.com>
2020-08-24 22:39:06 +0100

    test

---
+++ input-statement.py
@@ -0,0 +1,9 @@
+name = input("What is your Name : ") #input defult value is string
+print("My Name is :" ,name)
+
+value=input()
+print(type(value))
+
+mobile = int(input("Enter Your Mobile Number:"))
+print(mobile)
+print(type(mobile))
