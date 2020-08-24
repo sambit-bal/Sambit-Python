3675739883603ace025e6da672d8a714964464e7
sambit-bal <sambit.bal83@gmail.com>
2020-08-24 21:47:23 +0100

    test

---
+++ output-statement.py
@@ -0,0 +1,25 @@
+print("welcome to python exe")
+print()
+print('welcome to python exe' )
+
+print("Welcome")
+print("to")
+print("python exe")
+
+print("welcome", "to", "python sep", sep=' ' )  # sep 
+print("welcome", "to", "python sep", sep='***' )
+
+print("Welcome", end =' ') #end
+print("to", end=' ')
+print("python end")
+
+a= 10
+b= 20
+c= 30
+print(a,b,c, sep=' ')
+print(a,b,c, sep=',')
+print(a,b, sep=':')
+
+
+m= 40
+print("value:",m)
