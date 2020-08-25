101fd73a0dc1b5c6cd46836fecb564a4604611ae
sambit-bal <sambit.bal83@gmail.com>
2020-08-26 00:14:29 +0100

    test

---
+++ for-loop.py
@@ -0,0 +1,14 @@
+# For loop Simple
+# st= "Ireland"
+# for ch in st:
+#     print(ch)
+
+# print("Rest of the code")
+
+# For loop Range
+
+a= range(2, 20, 4)
+for i in a:
+    print(i)
+
+print("Rest of the code")
---
+++ range-function.py
@@ -0,0 +1,7 @@
+# Range() Function
+
+r = range(2,500,8)
+print(r[0],r[4],r[8], sep="\n")
+
+n = range(10, -200, -4)
+print(n[3],n[6],n[9], sep="\n")
