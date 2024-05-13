cc146b2e98c7629b093d2c7630341fc8bf84aeef
Sambit Bal <sambit.bal@ericsson.com>
2024-05-13 17:38:06 +0100

    hello file

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
 print("sambit")
 print("test for commit")
 print("again push")
+
+
+def square(x):
+   return x * x
+
+def sub(x, y):
+   return x - y
+
+print(sub(6,4))
+
+print(type (3))
+print(''' sam''')
+print(""" sam12 """)
+print(""" samb
+      sfjutn
+      thrye""")
